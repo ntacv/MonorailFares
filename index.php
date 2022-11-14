8bbed08ff9732187c182aa1b901bc4349ed8c135
ntacv <ntackn1@gmail.com>
2022-11-14 11:53:46 +0700

    Live share Co-authored-by: Kepler56 <Kepler56@users.noreply.github.com>

--- index.php
+++ index.php
@@ -34,62 +34,113 @@
         <?php
         $stations = array("KL Senral", "Tun Sambanthan", "Maharajalela", "Hang Tuah", "Imbi", "Bukit Bintang", "Raja Chulan", "Bukit Nanas", "Medan Tuanku", "Chow Kit", "Titiwangsa");
         $fares = array(
-            array(0,1.20,1.60,1.60,1.60,2.10,2.10,2.10,2.50,2.50,2.50),
-            array(1.20, 0, 1.20,1.60,1.60,1.60,2.10,2.10,2.10,2.50,2.50),
-            array(1.60,1.20,0,1.20,1.20,1.60,1.60,1.60,2.10,2.10,2.50),
-            array(1.60,1.60,1.20,0,1.20,1.20,1.20,1.60,1.60,2.10,2.10),
-            array(1.60,1.60,1.20,1.20,0,1.20,1.20,1.60,1.60,1.60,2.10),
-            array(2.10,1.60,1.60,1.20,1.20,0,1.20,1.20,1.60,1.60,2.10),
-            array(2.10,2.10,1.60,1.20,1.20,1.20,0,1.20,1.20,1.60,1.60),
-            array(2.10,2.10,1.60,1.60,1.60,1.20,1.20,0,1.20,1.20,1.60),
-            array(2.50,2.10,2.10,1.60,1.60,1.60,1.20,1.20,0,1.20,1.60),
-            array(2.50,2.50,2.10,2.10,1.60,1.60,1.60,1.20,1.20,0,1.20),
-            array(2.50,2.50,2.50,2.10,2.10,2.10,1.60,1.60,1.60,1.20,0)
+                    array(0, 1.20, 1.60, 1.60, 1.60, 2.10, 2.10, 2.10, 2.50, 2.50, 2.50),
+                    array(1.20, 0, 1.20, 1.60, 1.60, 1.60, 2.10, 2.10, 2.10, 2.50, 2.50),
+                    array(1.60, 1.20, 0, 1.20, 1.20, 1.60, 1.60, 1.60, 2.10, 2.10, 2.50),
+                    array(1.60, 1.60, 1.20, 0, 1.20, 1.20, 1.20, 1.60, 1.60, 2.10, 2.10),
+                    array(1.60, 1.60, 1.20, 1.20, 0, 1.20, 1.20, 1.60, 1.60, 1.60, 2.10),
+                    array(2.10, 1.60, 1.60, 1.20, 1.20, 0, 1.20, 1.20, 1.60, 1.60, 2.10),
+                    array(2.10, 2.10, 1.60, 1.20, 1.20, 1.20, 0, 1.20, 1.20, 1.60, 1.60),
+                    array(2.10, 2.10, 1.60, 1.60, 1.60, 1.20, 1.20, 0, 1.20, 1.20, 1.60),
+                    array(2.50, 2.10, 2.10, 1.60, 1.60, 1.60, 1.20, 1.20, 0, 1.20, 1.60),
+                    array(2.50, 2.50, 2.10, 2.10, 1.60, 1.60, 1.60, 1.20, 1.20, 0, 1.20),
+                    array(2.50, 2.50, 2.50, 2.10, 2.10, 2.10, 1.60, 1.60, 1.60, 1.20, 0)
+                );
+                $discount = array(
+                    array("name" => "Senior Citizens", "value" => 0.25),
+                    array("name" => "Disabled", "value" => 0.40),
+                    array("name" => "Students", "value" => 0.30)
         );
         ?>
 
 
 
-        <form action="Ex4-3Result.php" method="get">
+
+        <form action="" method="get">
             <br />
             <h3>Choose your trip</h3>
             <br />
 
-            <input type="radio" name="trip" value="1" checked> One-way
-            <input type="radio" name="trip" value="2"> Round-trip
+            <div class="wrapper d-flex">
+                <div class="custom-control custom-radio iconSelect mr-2">
+                    <input type="radio" id="customRadio1" name="customRadio" value="1" class="custom-control-input" checked>
+                    <label class="custom-control-label" for="customRadio1">
+                        One way
+                    </label>
+                </div>
 
+                <div class="custom-control custom-radio iconSelect ml-2">
+                    <input type="radio" id="customRadio2" name="customRadio" value="2" class="custom-control-input">
+                    <label class="custom-control-label" for="customRadio2">
+                        Return
+                    </label>
+                </div>
+            </div>
 
             <h3>From</h3>
-            <select name='selectWeight' required>
-                <option value=''>Select weight...</option>
+            <select name='selectDepartureStation' required>
+                <option value=''>Select departure station...</option>
                 <?php
-                for ($i = 0; $i < count($weight); $i++) {
-                    echo "<option value='$i'>" . $weight[$i] . "</option>";
+                for ($departureStation = 0; $departureStation < count($stations); $departureStation++) {
+                    echo "<option value='$departureStation'>" . $stations[$departureStation] . "</option>";
                 }
                 ?>
             </select>
             <br /><br />
 
-            To
-            <select name='selectCategory' required>
-                <option value=''>Select category...</option>
+            <h3>To</h3>
+            <select name='selectArrivalStation' required>
+                <option value=''>Select arrival station...</option>
                 <?php
-                for ($y = 0; $y < count($category); $y++) {
-                    echo "<option value='$y'>" . $category[$y] . "</option>";
+                for ($arrivalStation = 0; $arrivalStation < count($stations); $arrivalStation++) {
+                    echo "<option value='$arrivalStation'>" . $stations[$arrivalStation] . "</option>";
                 }
                 ?>
             </select>
             <br /><br />
             <h3>Discount</h3>
-            <select name='selectWeight'>
+            <select name='selectDiscount'>
                 <option value=''>Select a discount...</option>
                 <?php
-                for ($i = 0; $i < count($weight); $i++) {
-                    echo "<option value='$i'>" . $weight[$i] . "</option>";
+                for ($i = 0; $i < count($discount); $i++) {
+                    echo "<option value='$discount[$i]["val"]'>" . $discount[$i]["name"] . "</option>";
                 }
                 ?>
             </select>
             <br />
+
+
+            <div class="wrapper d-flex">
+                <div class="custom-control custom-radio iconSelect mr-2">
+                    <input type="radio" id="customRadio1" name="customRadio" value="1" class="custom-control-input" checked>
+                    <label class="custom-control-label" for="customRadio1">
+                        Adult
+                    </label>
+                </div>
+
+                <div class="custom-control custom-radio iconSelect ml-2">
+                    <input type="radio" id="customRadio2" name="customRadio" value="2" class="custom-control-input">
+                    <label class="custom-control-label" for="customRadio2">
+                        Student
+                    </label>
+                </div>
+
+                <div class="custom-control custom-radio iconSelect ml-2">
+                    <input type="radio" id="customRadio2" name="customRadio" value="2" class="custom-control-input">
+                    <label class="custom-control-label" for="customRadio2">
+                        Senior
+                    </label>
+                </div>
+
+                <div class="custom-control custom-radio iconSelect ml-2">
+                    <input type="radio" id="customRadio2" name="customRadio" value="2" class="custom-control-input">
+                    <label class="custom-control-label" for="customRadio2">
+                        Disabled
+                    </label>
+                </div>
+            </div>
+
+
             <br /><br />
             <input type="submit" value="Submit" class="primary-bg" />
         </form>
@@ -99,55 +150,31 @@
         <details>
             <summary>Show fule fares</summary>
             <p>
-                Table
-                <table>
-                    <tr>
-                        <th>From/To</th>
-                        <?php
-                            //echo "<table>";
-                            //echo "<tr>";
-                            //echo "<th>From/To</th>";
-                            for ($i = 0; $i < count($stations); $i++) {
-                                echo "<td>" . $stations[$i] . "</td>";
-                            }
-                            //echo "</tr>";
-                        ?>
-                    </tr>
-                    <tr>
-                        <?php
-                            for ($rows = 0; $rows < count($fares); $rows++) {
-                                //echo "<tr>";
-                                echo "<td>" . $stations[$rows] . "</td>";
-                                for ($col = 0; $col < count($fares[$rows]); $col++) {
-                                    echo "<td>" . $fares[$rows][$col] . "</td>";
-                                }
-                                //echo "</tr>";
-                            }
-                            //echo "</table>";
-                        ?>
-                    </tr>
-                </table>
+            <table>
+                <tr>
+                    <th>From \ To</th>
+                    <?php
+                for ($i = 0; $i < count($stations); $i++) {
+                    echo "<td>" . $stations[$i] . "</td>";
+                }
+                    ?>
+                </tr>
 
-
+                <?php
+                for ($rows = 0; $rows < count($fares); $rows++) {
+                    echo "<tr>";
+                    echo "<td>" . $stations[$rows] . "</td>";
+                    for ($col = 0; $col < count($fares[$rows]); $col++) {
+                        echo "<td>" . $fares[$rows][$col] . "</td>";
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </table>
             </p>
         </details>
 
 
-        <div class="wrapper d-flex">
-            <div class="custom-control custom-radio iconSelect mr-2">
-                <input type="radio" id="customRadio1" name="customRadio" value="1" class="custom-control-input" checked>
-                <label class="custom-control-label" for="customRadio1">
-                    email
-                </label>
-            </div>
-
-            <div class="custom-control custom-radio iconSelect ml-2">
-                <input type="radio" id="customRadio2" name="customRadio" value="2" class="custom-control-input">
-                <label class="custom-control-label" for="customRadio2">
-                    phone
-                </label>
-            </div>
-        </div>
 
 
     </section>
